39bf76703ed6c534b8bb1fbddbd217a7e15a1f58
winterom <winterom@gmail.com>
2023-05-26 17:17:56 +0300

    feat: Authentication

--- core/src/main/java/alexey/grizly/com/users/repositories/impl/EmailApprovedRepositoryImpl.java
+++ core/src/main/java/alexey/grizly/com/users/repositories/impl/EmailApprovedRepositoryImpl.java
@@ -1,12 +1,19 @@
 package alexey.grizly.com.users.repositories.impl;
 
 import alexey.grizly.com.users.repositories.EmailApprovedRepository;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EmailApprovedRepositoryImpl implements EmailApprovedRepository {
     private final String CHECK_TOKEN = "SELECT * FROM users as u left join email_approved_token as eat " +
             "on u.id = eat.userid where u.email=:email and eat.token=:token";
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public EmailApprovedRepositoryImpl(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int saveVerifiedEmailToken(final Long userId, final String token) {
         return 0;
